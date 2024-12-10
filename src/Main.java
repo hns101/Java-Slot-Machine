37c8ec4efff12f07415c721e81cd153fcea9e6e9
hns101 <hns101@live.nl>
2024-12-10 11:35:54 +0100

    Start of a slot machine

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JAVA's TOTALY NOT A SCAM SLOT MACHINE
+
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        System.out.println("///////////////Welcome To JAVA's////////////////"+"\n"+
+                           "////////Totally NOT A SCAM Slot Machine/////////"+"\n"+
+                           "/////////////// 🌟  🍒 🔔 🍉 🍋 //////////////");
+
+        while(balance > 0){
+
+        }
+
+        // declare var
+        // Display welcome
+        // play IF Balance > 0
+        // ENTER BET AMOUNT
+        //      Verify  if BET > Balance
+        //      Verify if  BET > 0
+        //      Subtract bet from Ballance
+        //Spin ROW
+        // PRINT ROW
+        // ASK TO PLAY AGAIN
+        // EXIT MESSAGE
+
+        scanner.close();
+
+    }
+}
